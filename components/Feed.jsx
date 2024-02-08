a51960735cc7f58844cd4f6eb2050d02bd7c7b4b
AbiNabat <154244797+AbiNabat@users.noreply.github.com>
2024-02-08 21:05:52 +0300

    main section(post)

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,28 @@
 import {SparklesIcon} from "@heroicons/react/24/outline";
 import Input from "@/components/Input";
+import Posts from "@/components/Posts";
 
 export default function Feed() {
+    const posts = [
+        {
+            id: "1",
+            name: "Dan",
+            username: "Dan",
+            userImg: "/user.png",
+            img: "/images/next.jpg",
+            text: "Nice view",
+            timestamp: "2 hours ago"
+        },
+        {
+            id: "2",
+            name: "Dan",
+            username: "Dan",
+            userImg: "/user.png",
+            img: "/images/one.jpg",
+            text: "Ago",
+            timestamp: "2 hours ago"
+        }
+    ]
     return (
         <div className='xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl'>
            <div className='flex py-2 px-3 sticky top-0 x-50 bg-white border-b border-gray-300'>
@@ -12,6 +33,9 @@ export default function Feed() {
             </div>
            </div>
             <Input/>
+            {posts.map((post) => (
+                <Posts key={post.id} post={post}/>
+            )) }
         </div>
     )
 }
